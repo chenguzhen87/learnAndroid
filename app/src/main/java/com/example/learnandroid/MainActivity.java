c9f61b0276eeaea2faffb6fae10b27dcb4607331
chenguzhen87 <546369005@qq.com>
2021-04-28 11:34:18 +0800

    chore: 布局管理器及点击事件

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.learnandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                //里面写点击后想要实现的效果
+                Toast.makeText(MainActivity.this,
+                        "按钮被点击", Toast.LENGTH_SHORT).show();
+                //这里是弹出一个消息---"按钮被点击"
+        }
+    }
+
 }
